fb451f9e28d62657d45910eb64ab5f765705f52a
LevanaChen <levana@levana-macbookpro.roam.corp.google.com>
2019-01-02 09:52:47 -0800

    Add verification script.

---
+++ Assets/Scripts/ARSessionVerification.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARExtensions;
+
+public class ARSessionVerification : MonoBehaviour
+{
+    private bool m_Verified = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!m_Verified && ARSubsystemManager.systemState >= ARSystemState.Ready)
+        {
+            Debug.Log("ARSessionVerification: " + ARSubsystemManager.sessionSubsystem);
+            Debug.Log("ARSessionVerification: " + ARSubsystemManager.sessionSubsystem.GetNativePtr());
+
+            m_Verified = true;
+        }
+
+    }
+}
